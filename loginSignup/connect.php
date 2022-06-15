7fa607b31ec04e245ccbcd46a43d6ba58cf909e4
Brian Galang <bryangalangoniel@gmail.com>
2022-06-15 12:17:23 +0800

    edited php

--- loginSignup/connect.php
+++ loginSignup/connect.php
@@ -1,23 +1,14 @@
 <?php
-    $name = $_POST['name'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-
-    //database connection
-    $conn = new mysqli('localhost', 'root','','db1');
-    if($conn->connect_error){
-        die('Connection Failed : '.$conn->connect_error);
-    }else{
-        $stmt = $conn->prepare("insert into registration_sample(name, password, email)
-            values(?, ?, ?)");
-        $stmt->bind_param("sss",$name, $password, $email);
-        $stmt->execute();
-        echo "Registration Successful";
-        $stmt->close();
-        $conn->close();
-    }
-
-
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
