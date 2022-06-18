744e5fe60b6ba14885a41fbf8f83065a40dd6b9f
Elthon <ejfcayetano.cca@gmail.com>
2022-06-18 18:53:59 +0800

    .

--- vendorDashboard/vendorDashboard.php
+++ vendorDashboard/vendorDashboard.php
@@ -30,18 +30,19 @@
                         <th>Water Type</th>
                         <th>Order Time</th>
                     </tr>
-                    <?php
-                    for($i=10; $i>=0; $i--) {?>
-                    <tr class="customerIteration">
-                        <td class="customerImage"><img src="/vendorDashboard/Resources/bona.jpg" alt=""></td>
-                        <td class="customerName">Elthon Jon F. Cayetano</td>
-                        <td class="customerArea">A</td>
-                        <td class="numberOfOrders">2</td>
-                        <td class="containerType">Round</td>
-                        <td class="waterType">Distilled</td>
-                        <td class="orderTime">12:59 pm</td>
-                    </tr>
-                    <?php } ?>
+                        <?php
+                        for($i=10; $i>=0; $i--) {?>
+                        <tr class="customerIteration">
+                            <td class="customerImage"><img src="/vendorDashboard/Resources/bona.jpg" alt=""></td>
+                            <td class="customerName">Elthon Jon F. Cayetano</td>
+                            <td class="customerArea">A</td>
+                            <td class="numberOfOrders">2</td>
+                            <td class="containerType">Round</td>
+                            <td class="waterType">Distilled</td>
+                            <td class="orderTime">12:59 pm</td>
+                        </tr>
+                        <?php } ?>
+                    
                 
                 </table>
                    
