0f9c92de763f6dc0c6bcfbd21a3f83d3b3bda906
Elthon <ejfcayetano.cca@gmail.com>
2022-06-18 18:08:30 +0800

    .

--- vendorDashboard/vendorDashboard.php
+++ vendorDashboard/vendorDashboard.php
@@ -21,16 +21,27 @@
 
         <section id="dashboard">
             <aside id="pull">
-                
-                <table class="pullOrder">
-                    <h1>Incoming Pull Orders</h1>
+                <h1>Incoming Pull Orders</h1>
+                <table style="width: 100%" >
+                    <tr class="headTable">
+                        <th colspan="2">Customer</th>
+                        <th>Area</th>
+                        <th colspan="2">Order Quantity</th>
+                        <th>Water Type</th>
+                        <th>Order Time</th>
+                    </tr>
+                    <?php
+                    for($i=10; $i>=0; $i--) {?>
                     <tr class="customerIteration">
-                        <td class="customerImage"><img src="" alt=""></td>
+                        <td class="customerImage"><img src="/vendorDashboard/Resources/bona.jpg" alt=""></td>
                         <td class="customerName">Elthon Jon F. Cayetano</td>
-                        <td class="customerArea">Area A</td>
-                        <td class="numberOfOrders">2 Round</td>
+                        <td class="customerArea">A</td>
+                        <td class="numberOfOrders">2</td>
+                        <td class="containerType">Round</td>
+                        <td class="waterType">Distilled</td>
                         <td class="orderTime">12:59 pm</td>
                     </tr>
+                    <?php } ?>
                 
                 </table>
                    
